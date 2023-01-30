b13b7c684ff452da2102ffbefba77405d96ae0f5
ignacio-001 <tsk7558@gmail.com>
2023-01-30 22:11:21 +0530

    Navigation bar changed

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,45 +1,42 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-const projectSchema=new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        default:'misc'
-    },
-    description:{
-        type:String,
-    },
-    free:{
-        type:Boolean,
-        require:true
-    },
-    price:{
-        type:Number,
-        default:'0'
-    },
-    technology:{
-        type:Object
-    },
-    video_link_free:{
-        type:String
-    },
-    //link available after purchase
-    video_link_paid:{
-        type:String
-    },
-    synopsis:{
-        type:Buffer
-    },
-    //available after purchase
-    full_report:{
-        type:Buffer
-    },
-    //paid user list is maitained for users who bought the course
-    paid_user:{
-        type:Array
-    }
-})
-module.exports=mongoose.model('Project',projectSchema);
+const projectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    default: "misc",
+  },
+  description: {
+    type: String,
+  },
+  free: {
+    type: Boolean,
+    require: true,
+  },
+  price: {
+    type: Number,
+    default: "0",
+  },
+  technology: {
+    type: Object,
+  },
+  video_link_free: {
+    type: String,
+  },
+  //link available after purchase
+  video_link_paid: {
+    type: String,
+  },
+  synopsis_url: {
+    type: String,
+  },
+  //available after purchase
+  full_report_url: {
+    type: String,
+  },
+  //paid user list is maitained for users who bought the course
+  paid_user: {
+    type: Array,
+  },
+});
--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,73 +3,85 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useLogout } from "./../hooks/useLogout";
 import { useAuthContext } from "./../hooks/useAuthContext";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
 
-const Nav = () => {
+const NavBar = () => {
   const { user, dispatch } = useAuthContext();
-  console.log(user);
   let authorized;
-  if(user!=null&&user.authority=='admin')authorized=true;
-  else authorized=false;
+  if (user != null && user.authority === "admin") authorized = true;
+  else authorized = false;
   const { logout } = useLogout();
   const handleClick = () => {
     logout();
   };
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#333",
-    color: "#fff",
-    padding: "10px 20px",
-  };
 
-  const linkStyle = {
-    color: "#fff",
-    textDecoration: "none",
-    marginRight: "10px",
-  };
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
-    if(user&&user.authority=='admin')authorized=true;
-    else authorized=false;
+    if (user && user.authority === "admin") authorized = true;
+    else authorized = false;
     if (!user) {
       console.log("no user logged in ");
     } else {
-      console.log(authorized)
+      console.log(authorized);
       console.log(user.email, "here");
       dispatch({ type: "LOGIN", payload: user });
       console.log("hello");
       console.log(user);
     }
   }, []);
+
   return (
-    <nav style={navStyle}>
-          <Link to="/" style={linkStyle}>
-            Home
-          </Link>
-      {user && (
-        <div>
-          <button onClick={handleClick}>Logout</button>
-        </div>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Home</Navbar.Brand>
+      {user && authorized && (
+        <Nav>
+          <Nav.Link as={Link} to="/createProject">Create Project</Nav.Link>
+        </Nav>
       )}
-      {user&&(authorized)&& (
-        <div>
-          <Link to="/createProject" style={linkStyle}>
-            Create_Project
-          </Link>
-        </div>
-      )}      
       {!user && (
-        <div>
-          <Link to="/login" style={linkStyle}>
+        <Nav>
+          <Nav.Link as={Link} to="/login">
             Login
-          </Link>
-          <Link to="/signup" style={linkStyle}>
-            Sign Up
-          </Link>
-        </div>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/signup">
+            Signup
+          </Nav.Link>
+        </Nav>
       )}
-    </nav>
+      <Nav className="mr-auto">
+        <Nav.Link href="#about-us">About Us</Nav.Link>
+        <NavDropdown title="Software" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#python">Python</NavDropdown.Item>
+          <NavDropdown.Item href="#data-science">Data Science</NavDropdown.Item>
+          <NavDropdown.Item href="#machine-learning">
+            Machine Learning
+          </NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Hardware" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#iot">IOT</NavDropdown.Item>
+          <NavDropdown.Item href="#robotics">Robotics</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      {user && (
+        <Nav>
+          <Button
+            variant="outline-light"
+            onClick={handleClick}
+            style={{ float: "right", marginLeft: 800 }}
+          >
+            Logout
+          </Button>
+        </Nav>
+      )}
+    </Navbar>
   );
 };
-export default Nav;
+
+export default NavBar;
